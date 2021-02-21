ca31f6f109b3633f0b87ad183f48ba796a078ab4
ing <shibin@yunzong.com>
2021-02-21 11:22:55 +0800

    update

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,42 @@
 // pages/category/index.js
+import {
+  request
+} from '../../request/index'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    leftMenuList: [],
+    rightMenuContent: []
   },
+  // 不放在data里考虑其不与渲染层交互，临时存储，省资源
+  categoryList: [],
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCategoryList()
   },
+  //  获取分类数据
+  getCategoryList() {
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/categories'
+    }).then(result => {
+      console.log(result);
+      this.categoryList = result.data.message
+      // 构造左侧大菜单数据
+      let leftMenuList = this.categoryList.map(v => v.cat_name)
+      let rightMenuContent = this.categoryList[0].children
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+      console.log(leftMenuList);
+      console.log(rightMenuContent);
+      this.setData({
+        leftMenuList,
+        rightMenuContent
+      })
+    })
   }
 })
