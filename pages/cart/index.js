d2a1369955f81f99d354ff76266ed2240ff013f7
ing <shibin@yunzong.com>
2021-02-25 15:10:41 +0800

    feat:购物车处理

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -12,11 +12,49 @@ Page({
     allChecked: false
   },
   onShow() {
-    let totalPrice = 0
-    let totalNum = 0
-    let allChecked = true
     const address = wx.getStorageSync('address')
     const cart = wx.getStorageSync('cart') || [];
+    this.setData({
+      address
+    })
+    this.updateCart(cart)
+  },
+  // 点击收货地址
+  handleChooseAddress() {
+    wx.chooseAddress({
+      success: (result) => {
+        result.all = result.provinceName + result.cityName + result.countyName + result.detailInfo
+        wx.setStorageSync('address', result)
+      }
+    });
+  },
+  // 商品选中
+  handleItemChange(e) {
+    // 获取被修改的商品的id
+    const goods_id = e.currentTarget.dataset.id
+    console.log(goods_id);
+    let {
+      cart
+    } = this.data
+    let index = cart.findIndex(v => v.goods_id === goods_id)
+    cart[index].checked = !cart[index].checked
+    this.updateCart(cart)
+  },
+  // 全选按钮
+  handleCheckedAll(e) {
+    let {
+      cart,
+      allChecked
+    } = this.data
+    allChecked = !allChecked
+    cart.forEach(v => v.checked = allChecked);
+    this.updateCart(cart)
+  },
+  // 设置购物车选中状态，重新计算
+  updateCart(cart) {
+    let allChecked = true
+    let totalPrice = 0
+    let totalNum = 0
     // 计算全选
     // every 数组方法 ：遍历 接收一个回调函数 每个回调函数都返回true 那么every方法返回true
     // 只要有一个回调函数返回false 就不再循环执行，直接返回false
@@ -33,26 +71,11 @@ Page({
     // 判断数组是否为空 因为如果为空是不会执行 forEach的
     allChecked = cart.length != 0 ? allChecked : false
     this.setData({
-      address,
       cart,
       allChecked,
       totalNum,
       totalPrice
     })
-  },
-  // 点击收货地址
-  handleChooseAddress() {
-    wx.chooseAddress({
-      success: (result) => {
-        result.all = result.provinceName + result.cityName + result.countyName + result.detailInfo
-        wx.setStorageSync('address', result)
-      }
-    });
-  },
-  // 复选框点击事件
-  handleItemChange(e) {
-    // 获取被修改的商品的id
-    const goods_id = e.currentTarget.dataset.id
-    console.log(goods_id);
+    wx.setStorageSync('cart', cart);
   }
 })
