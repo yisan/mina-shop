471893901433d924e50dac66c88267e760c99824
ing <shibin@yunzong.com>
2021-02-25 13:42:57 +0800

    feat:购物车

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,62 +5,54 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    address: {},
+    cart: [],
+    totalPrice: 0,
+    totalNum: 0,
+    allChecked: false
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onShow() {
+    let totalPrice = 0
+    let totalNum = 0
+    let allChecked = true
+    const address = wx.getStorageSync('address')
+    const cart = wx.getStorageSync('cart') || [];
+    // 计算全选
+    // every 数组方法 ：遍历 接收一个回调函数 每个回调函数都返回true 那么every方法返回true
+    // 只要有一个回调函数返回false 就不再循环执行，直接返回false
+    // 空数组 调用 every 返回值是false
+    // const allChecked = cart.length ? cart.every(v => v.checked) : false
+    cart.forEach(item => {
+      if (item.checked) {
+        totalPrice += item.goods_price
+        totalNum += item.num
+      } else {
+        allChecked = false
+      }
+    });
+    // 判断数组是否为空 因为如果为空是不会执行 forEach的
+    allChecked = cart.length != 0 ? allChecked : false
+    this.setData({
+      address,
+      cart,
+      allChecked,
+      totalNum,
+      totalPrice
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 点击收货地址
+  handleChooseAddress() {
+    wx.chooseAddress({
+      success: (result) => {
+        result.all = result.provinceName + result.cityName + result.countyName + result.detailInfo
+        wx.setStorageSync('address', result)
+      }
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 复选框点击事件
+  handleItemChange(e) {
+    // 获取被修改的商品的id
+    const goods_id = e.currentTarget.dataset.id
+    console.log(goods_id);
   }
 })
--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -10,16 +10,45 @@ Page({
   data: {
     goodsInfo: {}
   },
+  GoodsInfo: {},
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options);
     const {
       goods_id
     } = options
     this.getGoodsDetail(goods_id)
+  },
+  // 点击轮播图，大图预览
+  handlePreviewImage() {
+    // 先构造要预览的图片数组
+    const urls = this.GoodsInfo.pics.map(v => v.pics_mid)
+    wx.previewImage({
+      current: urls[0],
+      urls: urls,
+
+    });
+  },
+  // 添加到购物车
+  handleCartAdd() {
+    let cart = wx.getStorageSync('cart') || []
+    let index = cart.findIndex(v => v.goods_id === this.GoodsInfo.goods_id)
+    if (index === -1) {
+      // 不存在 第一次添加
+      this.GoodsInfo.num = 1
+      this.GoodsInfo.checked = true
+      cart.push(this.GoodsInfo)
+    } else {
+      cart[index]++
+    }
+    wx.setStorageSync('cart', cart);
+    wx.showToast({
+      title: '添加成功',
+      icon: 'none',
+      mask: true
+    });
   },
   async getGoodsDetail(goods_id) {
     const goodsInfo = await request({
@@ -28,8 +57,19 @@ Page({
         goods_id
       }
     })
+    this.GoodsInfo = goodsInfo
+    console.log(goodsInfo);
     this.setData({
-      goodsInfo
+      goodsInfo: {
+        goods_name: goodsInfo.goods_name,
+        goods_price: goodsInfo.goods_price,
+        // iphone部分手机，不识别webp图片格式
+        // 最后后台修改
+        // 临时 ： 确保后台存在 1.webp => 1.jpg
+        // goods_introduce: goodsInfo.goods_introduce,
+        goods_introduce: goodsInfo.goods_introduce.replace(/\.webp/g, '.jpg'),
+        pics: goodsInfo.pics
+      }
     })
   }
 })
